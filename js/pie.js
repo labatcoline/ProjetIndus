03715d89840a5c85183453f0d2edc9423d81e912
labatcoline <coline.labat@Colines-iMac.local>
2016-04-15 10:37:04 +0200

    creation pie.js

--- js/pie.js
+++ js/pie.js
@@ -10,5 +10,21 @@ function draw(data){
   var nameDim = ndx.dimension(function(d) {return d.name})
   //Select the value for all the dimensions
   var valuesDim = nameDim.group().reduceSum(function(d) {return d.value});
-  
+
+  //create the pie
+  var pieChart = dc.pieChart('#chart')
+    .width(768)
+    .height(480)
+    .slicesCap(100) //Je ne sais pas ce que ça fait...
+    .innerRadius(50) //Pour afficher un rond au milieu du graphique
+    .dimension(nameDim) //axe x
+    .group(valuesDim)       // axe y
+    .legend(dc.legend().gap(3))
+
+    dc.renderAll()                //affiche le graphique
+}
+
+function type(d) {
+  d.value = +d.value; // coerce to number
+  return d;
 }
